e41d32079ec5c6c9a83b88e9e1b8bd00fa4dadf1
William de Montmollin <awdem@MASTAFF-Joe-Charm-C17GMB2AQ6L4.local>
2023-04-13 14:21:05 +0100

    finished uppercase method design exercise

--- lib/uppercase.rb
+++ lib/uppercase.rb
@@ -1,7 +1,10 @@
 def extract_uppercase(str)
-  uppercase = str.split
-  uppercase.select do |word|
+  fail "Nothing there!" if str.nil?
+  words = str.split
+  uppercase_words = words.select do |word|
     word == word.upcase
   end
-
+  uppercase_words.map do |word|
+    word.gsub(/\W/, '')
+  end 
 end
--- spec/uppercase_spec.rb
+++ spec/uppercase_spec.rb
@@ -7,8 +7,38 @@ RSpec.describe "extract_uppercase" do
         expect(result).to eq []
     end
  end 
+
     it "given a string, returns uppercase words" do
         result =  extract_uppercase("HELLO WORLD")
         expect(result).to eq ["HELLO", "WORLD"]
     end
+
+  context "given a string with mixed cases" do
+    it "returns an array of only uppercase words" do
+      result = extract_uppercase("HELLO world")
+      expect(result).to eq ["HELLO"]
+    end
+  end
+
+  context "given a string with only lowercase words" do
+    it "returns an empty array" do
+      result = extract_uppercase("hello world")
+      expect(result).to eq []
+    end
+  end
+
+
+  context "given a string with uppercase words and exclamation marks" do
+    it "returns only uppercase words" do
+      result = extract_uppercase("HELLO! WORLD!")
+      expect(result).to eq ["HELLO", "WORLD"]
+    end
+  end
+
+  context "given nil" do
+    it "fails" do
+      expect { extract_uppercase(nil) }.to raise_error "Nothing there!"
+    end
+  end
+
 end
